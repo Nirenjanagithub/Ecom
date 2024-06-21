d8753931c064f585920b56f0070bf0edae992854
Nirenjana <unirenjana@gmail.com>
2024-06-21 11:34:27 +0530

    Update ecomcategories.js

--- Ecomjs/ecomcategories.js
+++ Ecomjs/ecomcategories.js
@@ -1,57 +1,51 @@
-const apiUrl7 ="https://fakestoreapi.com/products/categories";
+const apiUrl7 = "https://fakestoreapi.com/products/categories";
 fetch(apiUrl7)
-.then(response=>{
-    if(!response.ok){
+.then(response=> {
+    if(!response.ok) {
         throw new Error("Error converting the json data")
     }
-    return response.json()
+    return response.json();
 })
-.then(data=>{
+.then(data=> {
     displayCategories(data);
 })
 .catch(error=> console.error("Error fetching data",error));
 
-function displayCategories(items){
-    const categorybody = document.getElementById('categorybody');
-    items.forEach(item=>{
-        const elements =document.createElement('div');
-        elements.innerHTML=`
-        <button id="catbutton">${item}</button>
-        
-        `;
-        categorybody.appendChild(elements)
+function displayCategories(items) {
+    const categoryBody = document.getElementById('categorybody');
+    items.forEach(item=> {
+        const elements = document.createElement('div');
+        elements.innerHTML= `
+            <button id = "catbutton">${item}</button>`;
+        categoryBody.appendChild(elements);
     })
     
 }
-document.getElementById('categorybody').addEventListener('click',()=>{
+document.getElementById('categorybody').addEventListener('click', () => {
     const apiUrl8 = 'https://fakestoreapi.com/products/category/jewelery';
     fetch(apiUrl8)
-    .then(response=>{
+    .then(response=> {
         if(!response.ok){
             throw new Error("Error converting the json data")
         }
-        return response.json()
+        return response.json();
     })
-    .then(data=>{
+    .then(data=> {
         displayProducts(data);
     })
     .catch(error=> console.error("Error fetching data",error));
 })
-function displayProducts(characters){
+function displayProducts(characters) {
     const tableBody = document.getElementById('catbody');
-
-    characters.forEach(character=>{
-        const row =document.createElement('tr');
-        row.innerHTML=`
-            <td><img src="${character.image}"alt="${character.title}" width="30%"></td>
-            <td><h4><a href="ecomproduct.html" onclick="show()" target="_blank">${character.title}</a></h4></td>
+    characters.forEach(character=> {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td><img src = "${character.image}"alt = "${character.title}" 
+            width = "30%"></td>
+            <td><h4><a href = "ecomproduct.html" onclick = "show()" 
+            target = "_blank">${character.title}</a></h4></td>
             <td><h4>COST:$${character.price}</h4></td>
-          
-
-           `;
-           tableBody.appendChild(row)
-          
-          
-      
-    });
+        `;
+        tableBody.appendChild(row);
+    })
 }
